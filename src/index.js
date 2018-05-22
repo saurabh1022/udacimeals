1c8efdaca34dff7e985f5cdce98d01efe879a76e
saurabh1022 <201552051@iiitvadodara.ac.in>
2018-05-22 10:06:20 +0530

    All files committed

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker';
+
+const logger = store => next => action => {
+	console.group(action.type)
+	console.info('dispatching',action)
+	let result = next(action)
+	console.log('next state', store.getState())
+	console.groupEnd(action.type)
+	return result
+}
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(
+	reducer,
+	composeEnhancers(
+			applyMiddleware(logger)
+		)
+	)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
